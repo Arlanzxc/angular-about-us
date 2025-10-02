03a057ee421fbc5855aab0fcd253101307363a58
Arlan Maximov <maxarlan974@gmail.com>
2025-10-02 23:44:45 +0500

    Updated page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
@@ -8,6 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    importProvidersFrom(FormsModule)
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  title = "Student of KBTU";
+  greeting = "Hi, I'm Arlan Maximov!";
+
+  photoUrl = "/user.jpg";
+  isButtonDisabled = false;
+
+  likes = 0;
+  showMessage = false;
+  
+  addLike(){
+    this.likes++;
+  }
+
+  toggleMessage(){
+    this.showMessage = !this.showMessage;
+  }
+
+  name = "";
+  email = "";
+  subscribed = false;
+
+  subscribe(){
+    if(this.email){
+    this.subscribed = true;
+  }
+}
+
+  activeTab: string = 'hobbies'; 
+
+  opentab(tabname: string) {
+    this.activeTab = tabname;
+  }
 }
